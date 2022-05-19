185958a4162640aa974e94c6033fff762b604c5d
unknown <raushankumar500009@gmail.com>
2022-05-19 16:12:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Routes, Route } from "react-router-dom";
+import RegiForm from "./components/RegiForm/RegiForm";
+import './App.scss';
+import Bar from "./components/Graph/Bar";
+
+function App() {
+  return (
+      <Routes>        
+        <Route path="/" element={< RegiForm />} />
+        <Route path="/bar" element={< Bar />} />
+      </Routes>
+  
+  );
+}
+
+export default App;
